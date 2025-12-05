ae14b5216301d40476e6eb292bd9dadcfa2049d4
Yuvan Chikka <147429542+Ultraviolet-Chikorita@users.noreply.github.com>
2025-12-05 20:03:37 +0000

    Update Django settings with allowed hosts

--- workshop3/workshop3/settings.py
+++ workshop3/workshop3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-js91730em-k=f*krm*75md7tmob927*@y=8m50m#rde7b6&zim
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["workshop04.onrender.com"]
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
